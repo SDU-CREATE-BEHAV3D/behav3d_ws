871d478b350e179d1bd04fb1cb549617aab3ba42
Lucas-Helle <lucas.helle.p@gmail.com>
2025-07-29 13:36:36 +0200

    pilz_demo launch config + setup.cfg edit

--- src/pilz_demo/setup.py
+++ src/pilz_demo/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+import os
+from glob import glob
 
 package_name = 'pilz_demo'
 
@@ -8,13 +10,15 @@ setup(
     packages=find_packages(include=[package_name, package_name + '.*']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),        # ← resource-index marker
+            ['resource/' + package_name]),        # Marker
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='you@example.com',
+    maintainer='Lucas José Helle',
+    maintainer_email='luh@iti.sdu.dk',
     description='Interactive demo that converts text commands into Pilz-planned motions and visualises them.',
     license='MIT',
     tests_require=['pytest'],
