228c49f874138758791a054a708fb66320d147e0
Ozguc Bertug Capunaman <ozgucbertug@gmail.com>
2025-07-29 13:05:53 +0200

    Update setup.py

--- src/pilz_demo/setup.py
+++ src/pilz_demo/setup.py
@@ -4,19 +4,23 @@ package_name = 'pilz_demo'
 
 setup(
     name=package_name,
-    version="0.0.1",
-    packages=find_packages(include=[package_name, package_name + ".*"]),
-    data_files=[("share/" + package_name, ["package.xml"])],
-    install_requires=["setuptools"],
+    version='0.0.1',
+    packages=find_packages(include=[package_name, package_name + '.*']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),        # ← resource-index marker
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer="Tu Nombre",
-    maintainer_email="tu@mail.com",
-    description="Dummy setup.py for building",
-    license="MIT",
-    tests_require=["pytest"],
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='Interactive demo that converts text commands into Pilz-planned motions and visualises them.',
+    license='MIT',
+    tests_require=['pytest'],
     entry_points={
-        "console_scripts": [
-            "run_demo = pilz_demo.demo:main",
+        'console_scripts': [
+            'run_demo = pilz_demo.demo:main',
         ],
     },
 )
