edaa69a02ff6de7b50947fe9de674fb5a743ab5f
Lucas-Helle <lucas.helle.p@gmail.com>
2025-07-29 10:51:46 +0200

    setup.py dummy files for compiling

---
+++ src/pilz_motion_controller/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+package_name = 'pilz_motion_controller'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Luc',
+    maintainer_email='tu@mail.com',
+    description='Dummy setup.py for building',
+    license='MIT',
+    tests_require=['pytest'],
+)
