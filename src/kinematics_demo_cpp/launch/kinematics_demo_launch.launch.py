9f032e71a4d7fc18c031187805e9385ef0300bf5
Lucas-Helle <lucas.helle.p@gmail.com>
2025-07-31 18:51:38 +0200

    Update kinematics_demo_launch.launch.py

--- src/kinematics_demo_cpp/launch/kinematics_demo_launch.launch.py
+++ src/kinematics_demo_cpp/launch/kinematics_demo_launch.launch.py
@@ -120,6 +120,8 @@ def generate_launch_description():
             moveit_config.robot_description,
             moveit_config.robot_description_semantic,
             moveit_config.robot_description_kinematics,
+            moveit_config.planning_pipelines,
+            moveit_config.joint_limits,
         ],
     )
 
