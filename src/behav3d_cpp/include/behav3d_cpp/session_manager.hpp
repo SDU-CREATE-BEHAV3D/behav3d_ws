2d0978622de797568b2670bb1afbec13158328ac
Ozguc Bertug Capunaman <ozgucbertug@gmail.com>
2025-08-12 17:37:10 +0200

    Update session_manager.hpp

--- src/behav3d_cpp/include/behav3d_cpp/session_manager.hpp
+++ src/behav3d_cpp/include/behav3d_cpp/session_manager.hpp
@@ -1,4 +1,18 @@
-#pragma once
+// =============================================================================
+//   ____  _____ _   _    ___     _______ ____
+//  | __ )| ____| | | |  / \ \   / /___ /|  _ \ 
+//  |  _ \|  _| | |_| | / _ \ \ / /  |_ \| | | |
+//  | |_) | |___|  _  |/ ___ \ V /  ___) | |_| |
+//  |____/|_____|_| |_/_/   \_\_/  |____/|____/
+//
+// Author:
+//   - Özgüç Bertuğ Çapunaman <ozca@iti.sdu.dk>
+// Maintainers:
+//   - Lucas José Helle <luh@iti.sdu.dk>
+//   - Joseph Milad Wadie Naguib <jomi@iti.sdu.dk>
+// Institute: University of Southern Denmark (Syddansk Universitet)
+// Date: 2025-08
+// =============================================================================
 
 #include <memory>
 #include <optional>
