b01db977b0e93de6ddcb79844ecd2cb6b7774cb9
Lucas-Helle <lucas.helle.p@gmail.com>
2026-02-11 14:13:53 +0100

    new hand eye and some stuff in my scripts in tsdf

--- python_scripts/handeye_calibration/handeye_v4.py
+++ python_scripts/handeye_calibration/handeye_v4.py
@@ -12,13 +12,13 @@ from utils.transforms import rotmat_to_quat_xyzw, rotmat_to_rpy,compose_T
 from utils.intrinsics import load_intrinsics, intrinsics_matrix
 from utils.extrinsics import load_extrinsics
 
-DEBUG_IR = False
-DEBUG_COLOR = True
+DEBUG_IR =  False
+DEBUG_COLOR = False
 VALIDATION = True
 
-SESSION_PATH = "/home/lab/behav3d_ws/captures/260113_170839"
+SESSION_PATH = "/home/lab/behav3d_ws/captures/260211_133218/"
 
-scan_folder = "manual_caps"
+scan_folder = "manual_caps2"
 
 my_session = Session(SESSION_PATH, scan_folder)
 
@@ -181,7 +181,7 @@ def main():
 
     # IR Execute detection and Collect ir pairs 
 
-    skip_idx = {10}  # Index to skip
+    skip_idx = {}  # Index to skip
 
     for i, p in enumerate(ir_img_path):
         if i in skip_idx:
