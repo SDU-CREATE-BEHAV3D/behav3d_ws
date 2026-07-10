48a6ac1dc5f1b1d881523f076ac0c3ab4ff2047a
Lucas-Helle <lucas.helle.p@gmail.com>
2026-07-10 12:36:56 +0200

    Bring dev_joe half cylinder scan updates

--- src/behav3d_orchestrator/behav3d_orchestrator/src/scan_sequences/__init__.py
+++ src/behav3d_orchestrator/behav3d_orchestrator/src/scan_sequences/__init__.py
@@ -1,5 +1,5 @@
 """Scan target generators used by ScanSession."""
 
-from . import fibonacci, grid_sweep, half_cylinder
+from . import fibonacci, grid_sweep, half_cylinder, half_cylinder_side_caps
 
-__all__ = ["fibonacci", "grid_sweep", "half_cylinder"]
+__all__ = ["fibonacci", "grid_sweep", "half_cylinder", "half_cylinder_side_caps"]
