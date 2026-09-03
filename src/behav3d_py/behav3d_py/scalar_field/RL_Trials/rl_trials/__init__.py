4b38ec38ca605bb593157fc61805645e48f8ef5e
Lucas-Helle <91989399+Lucas-Helle@users.noreply.github.com>
2026-09-03 19:17:49 +0200

    Expose stability reward terms

--- src/behav3d_py/behav3d_py/scalar_field/RL_Trials/rl_trials/__init__.py
+++ src/behav3d_py/behav3d_py/scalar_field/RL_Trials/rl_trials/__init__.py
@@ -14,6 +14,7 @@ from .goal_evaluator import (
     evaluate_goal_with_vertical_rays,
     vertex_area_weights,
 )
+from .rewards import cantilever_ratio_cost, normalized_tilt_cost
 from .visualization import EvaluationScene, build_evaluation_scene
 
 __all__ = [
@@ -24,9 +25,11 @@ __all__ = [
     "GoalMetrics",
     "EvaluationScene",
     "build_evaluation_scene",
+    "cantilever_ratio_cost",
     "decode_continuous_action",
     "evaluate_goal_from_height_samples",
     "evaluate_goal_with_vertical_rays",
+    "normalized_tilt_cost",
     "sample_action_candidates",
     "validate_action_candidates",
     "vertex_area_weights",
